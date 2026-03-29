4c42bf82b0bc632d6405b44939e896a8c1c254f6
Xzayler <csak.mityu@gmail.com>
2026-03-29 19:21:22 +0200

    Add db functions

--- worker/src/db/db.rs
+++ worker/src/db/db.rs
@@ -1,36 +1,105 @@
-use sqlx::{Postgres, postgres::PgPoolOptions, Pool};
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use std::env;
-use super::model::Job;
-use super::model::JobStatus;
-use super::model::JobType;
-use super::model::JobInputType;
+use super::model::{Job, JobId};
 
-pub async fn get_job(pool: &sqlx::PgPool) -> Result<Job, sqlx::Error> {
-  let job = sqlx::query_as!(
-    Job,
-    "SELECT
-    id,
-    user_id,
-    status AS \"status: JobStatus\",
-    job_type AS \"job_type: JobType\",
-    input_type AS \"input_type: JobInputType\",
-    input_uri,
+pub async fn pick_pending_job(pool: &sqlx::PgPool) -> Result<Option<JobId>, sqlx::Error> {
+  // TODO: Update started_at to now
+  let job_id: Option<JobId> = sqlx::query_as::<Postgres, JobId>(
+    "UPDATE jobs 
+    SET
+      status = 'Running',
+      started_at = NOW()
+    WHERE id = (
+      SELECT id
+      FROM jobs
+      WHERE status = 'Pending'
+      ORDER BY created_at ASC
+      FOR UPDATE SKIP LOCKED
+      LIMIT 1
+    )
+    RETURNING id;
+  ").fetch_optional(pool).await?;
+  
+  Ok(job_id)
+}
+
+pub async fn get_job(pool: &sqlx::PgPool, id: i32) -> Result<Job, sqlx::Error> {
+  let job: Job = sqlx::query_as(
+    format!("SELECT
+    jobs.id,
+    status,
+    job_type,
+    norm,
     output_uri,
-    created_at,
     started_at,
-    finished_at
+    finished_at,
+    systems.id as system_id,
+    systems.dimension,
+    systems.base,
+    systems.digit_type,
+    systems.is_gns,
+    systems.signature,
+    systems.last_job,
+    systems.digit_param,
+    (
+      SELECT array_agg(v.elements)
+      FROM digits v
+      WHERE v.id = ANY(systems.digits)
+    )::text as digits
     FROM jobs
-    WHERE status='Pending'",
+    JOIN systems ON jobs.system_id = systems.id
+    WHERE jobs.id = {id}").as_str()
   )
   .fetch_one(pool)
-  .await
-  .unwrap();
+  .await?;
 
   Ok(job)
 }
 
-pub async fn connect() -> Pool<Postgres> {
+pub async fn update_db_with_results(pool: &sqlx::PgPool, job_id: i32, system_id: i32, is_gns: Option<bool>, signature: Option<Vec<i32>>) -> Result<(),sqlx::Error> {
+  let mut transaction = pool.begin().await?;
+  
+  // TODO: Walk results
+  sqlx::query(
+    format!("UPDATE jobs
+    SET status = 'Succeeded',
+      finished_at = NOW()
+    WHERE id = {job_id}"
+    ).as_str()
+  )
+  .execute(&mut *transaction)
+  .await?;
+  
+  let set_gns_string = match is_gns {
+    Some(gns) => format!("is_gns = {gns},"),
+    None => "".to_string()
+  };
 
+  let set_signature_string = match signature {
+    Some(sig) => {
+      let sig_str = vec_to_sql_string(sig);
+      format!("signature = ARRAY{sig_str},")
+    },
+    None => "".to_string()
+  };
+  sqlx::query(
+    format!("UPDATE systems
+    SET 
+    {set_gns_string}
+    {set_signature_string}
+    last_job = NOW()
+    WHERE id = {system_id}
+    ").as_str()
+  )
+  .execute(&mut *transaction)
+  .await?;
+
+  transaction.commit().await?;
+
+  Ok(())
+}
+
+pub async fn connect() -> Result<Pool<Postgres>, sqlx::Error> {
   let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
   PgPoolOptions::new()
@@ -38,5 +107,9 @@ pub async fn connect() -> Pool<Postgres> {
     .min_connections(1)
     .connect(&database_url)
     .await
-    .expect("Failed to create pool.")
+}
+
+fn vec_to_sql_string(vec: Vec<i32>) -> String {
+  let str_vec: Vec<String> = vec.into_iter().map(|e| e.to_string()).collect();
+  "[".to_string() + &str_vec.join(",") + "]"
 }
--- worker/src/db/model.rs
+++ worker/src/db/model.rs
@@ -1,12 +1,39 @@
 use std::option::Option;
-use sqlx::types::chrono::{DateTime, Utc};
+use sqlx::{FromRow, Row, postgres::PgRow, types::chrono::{DateTime, Utc}};
+
+#[derive(Debug, sqlx::Type)]
+#[sqlx(type_name = "digit_type", rename_all = "PascalCase")]
+pub enum DigitType {
+  Explicit,
+  Canonical,
+  JCanonical,
+  Dense,
+  Adjoined,
+  Symmetric,
+  JSymmetric,
+  Shifted
+}
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct System {
+  pub id: i32,
+  pub dimension: i32,
+  pub base: Vec<i32>,
+  pub digit_type: DigitType,
+  pub is_gns: Option<bool>,
+  pub signature: Option<Vec<i32>>,
+  pub last_job: Option<DateTime<Utc>>,
+  // digit fields
+  pub digit_param: Option<i32>,
+  pub digits: Option<Vec<Vec<i32>>>
+}
 
 #[derive(Debug, sqlx::Type)]
 #[sqlx(type_name = "status")]
 pub enum JobStatus {
   Pending,
   Running,
-  Completed,
+  Succeeded,
   Failed
 }
 
@@ -19,27 +46,85 @@ pub enum JobType {
 }
 
 #[derive(Debug, sqlx::Type)]
-#[sqlx(type_name = "input_type")]
-pub enum JobInputType {
-  Custom,
-  Canonical,
-  JCanonical,
-  Dense,
-  Adjoined,
-  Symmetric,
-  Shifted
+#[sqlx(type_name = "norm_type")]
+pub enum NormType {
+  Infinite,
+  L1,
+  L2
 }
 
-#[derive(sqlx::FromRow)]
+#[derive(Debug)]
 pub struct Job {
   pub id: i32,
-  pub user_id: i32,
+  pub system: System,
   pub status: JobStatus,
+  pub norm: NormType,
   pub job_type: JobType,
-  pub input_type: JobInputType,
-  pub input_uri: String,
   pub output_uri: Option<String>,
-  pub created_at: Option<DateTime<Utc>>,
   pub started_at: Option<DateTime<Utc>>,
   pub finished_at: Option<DateTime<Utc>>
+}
+
+impl FromRow<'_, PgRow> for Job {
+  fn from_row(row: &PgRow) -> sqlx::Result<Self> {
+    
+    let digits: Option<Vec<Vec<i32>>> = match row.get::<Option<String>, &str>("digits") {
+      Some(str_value) => {
+        Some(parse_vector_of_vectors(&str_value))
+      },
+      None => None
+    };
+    
+    let system = System {
+      id: row.get("system_id"),
+      dimension: row.get("dimension"),
+      base: row.get("base"),
+      digit_type: row.get("digit_type"),
+      is_gns: row.get("is_gns"),
+      signature: row.get("signature"),
+      last_job: row.get("last_job"),
+      // digit fields
+      digit_param: row.get("digit_param"),
+      digits: digits
+    };
+    Ok(Self {
+      id: row.get("id"),
+      system: system,
+      status: row.get("status"),
+      job_type: row.get("job_type"),
+      norm: row.get("norm"),
+      output_uri: row.get("output_uri"),
+      started_at: row.get("started_at"),
+      finished_at: row.get("finished_at")
+    })
+  }
+}
+
+
+pub fn parse_vector_of_vectors(s: &str) -> Vec<Vec<i32>> {
+  // if !s.starts_with("{{") || !s.ends_with("}}") {
+  //     return Err("Invalid format: string must start with {{ and end with }}".into());
+  // }
+  let inner = &s[1..s.len() - 1]; // remove outer {}
+  let parts: Vec<&str> = inner.split("},{").collect();
+  let mut result = Vec::new();
+  for part in parts {
+    let cleaned = part.trim_matches('{').trim_matches('}');
+    let nums: Vec<i32> = cleaned.split(',')
+      .map(|n| {
+          match n.trim().parse::<i32>() {
+            Ok(res)=> res,
+            Err(_) => 99
+          }
+        }
+      )
+      .collect();
+    result.push(nums);
+  }
+  result
+}
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct JobId {
+  pub id: i32
 }
